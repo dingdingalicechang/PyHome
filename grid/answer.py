7cd0291dbaf6517a4038525c7d22bc9115831d68
dingdingc <wenchia79819@gmail.com>
2023-03-24 15:07:59 +0800

    sleepy

---
+++ grid/answer.py
@@ -0,0 +1,50 @@
+#Teacher's version at this point
+
+import tkinter as tk
+from tkinter import ttk
+
+class Window(tk.Tk):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        ttkStyle = ttk.Style()
+        ttkStyle.theme_use('default')
+        ttkStyle.configure('red.TFrame',background='#ff0000')
+        ttkStyle.configure('white.TFrame',background='#ffffff')
+        ttkStyle.configure('yellow.TFrame',background='yellow')
+        ttkStyle.configure('white.TLabel',background='#ffffff')
+        ttkStyle.configure('gridLabel.TLabel',font=('Helvetica', 16))
+        ttkStyle.configure('gridEntry.TEntry',font=('Helvetica', 16))
+        
+        mainFrame = ttk.Frame(self)        
+        mainFrame.pack(expand=True,fill=tk.BOTH)
+        
+
+        topFrame = ttk.Frame(mainFrame,height=100)
+        topFrame.pack(fill=tk.X)
+
+        ttk.Label(topFrame,text="BMI試算",font=('Helvetica', '20')).pack(pady=20)
+
+        bottomFrame = ttk.Frame(mainFrame,style='yellow.TFrame')
+        bottomFrame.pack(expand=True,fill=tk.BOTH)
+        bottomFrame.columnconfigure(0,weight=3)
+        bottomFrame.columnconfigure(1,weight=5)
+        bottomFrame.rowconfigure(0, weight=1)
+
+        ttk.Label(bottomFrame,text="姓名:",style='gridLabel.TLabel').grid(column=0,row=0,sticky=tk.E)
+        ttk.Entry(bottomFrame,style='gridEntry.TEntry').grid(column=1,row=0,sticky=tk.W)
+
+
+
+
+
+def main():
+    '''
+    這是程式的執行點
+    '''
+    window = Window()
+    window.title("BMI計算")
+    window.geometry("400x500")
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
--- grid/index.py
+++ grid/index.py
@@ -9,24 +9,40 @@ class Windows(tk.Tk):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
         ttkStyle =ttk.Style()
-        print(ttkStyle.theme_names())
         ttkStyle.theme_use("default")
         ttkStyle.configure("1.TFrame",background = "#008080")
         ttkStyle.configure("2.TFrame",background= "#ff7f50") 
-        ttkStyle.configure("3.TFrame",background="#ffffff")
+        ttkStyle.configure("3.TFrame",background= "#ffffff")
+        ttkStyle.configure("1.TLabel",background= "#3cb371")
         
-        mainFrame = ttk.Frame(self,style="1.TFrame")
+        mainFrame = ttk.Frame(self)
         mainFrame.pack(expand = True, fill=tk.BOTH)
 
         '''
         ttk.Label(mainFrame,text="BMI試算").pack()
         '''
 
-        topFrame = ttk.Frame(mainFrame, style="1.TFrame",height=100)
+        topFrame = ttk.Frame(mainFrame,style="1.TFrame",height=100)
         topFrame.pack(fill=tk.X)
+
+        ttk.Label(topFrame,text="BMI calculate", style="1.TLabel").pack(pady=25)
 
         bottomFrame = ttk.Frame(mainFrame,style="2.TFrame")
         bottomFrame.pack(expand=True,fill=tk.BOTH)
+        '''
+        --新增這個以後我的style=1.TLabel會失效
+        bottomFrame.pack(expand=True,fill=tk.BOTH)
+        bottomFrame.columnconfigure(0,weight=3)
+        bottomFrame.columnconfigure(1,weight=5)
+        bottomFrame.rowconfigure(0, weight=1)
+        '''
+        
+        
+
+        ttk.Label(bottomFrame,text="name",style="gridLabel.TLabel").grid(column=0,row=0,sticky=tk.E)
+        ttk.Entry(bottomFrame).grid(column=1,row=0, sticky=tk.W)
+
+        
 
 
 
@@ -40,9 +56,6 @@ def main():
     window.title("BMI calculator")
     window.geometry("400x500") 
     window.mainloop()
-
-
-    pass
 
 if __name__=="__main__":
     main()
